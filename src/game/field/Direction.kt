a7bdb8176a4e411898ce20288080a66621665556
phinesze <phinesze@gmail.com>
2018-09-11 08:10:04 +0900

    DirectionにコメントとisHorizonal/isVerticalを追加

--- src/game/field/Direction.kt
+++ src/game/field/Direction.kt
@@ -1,5 +1,10 @@
 package game.field
 
+/**
+ * 上下左右の方向を表す列挙体。フィールドの矢印レイヤー(FieldArrowLayer)で使用される。
+ * 「矢印がない」ことを表す"none"は方向には存在しない。
+ * Directionが特定の方向を指定する場合に使用するのに対し、Arrowはフィールド上のブロックに隣接する矢印オブジェクトを表す。
+ */
 enum class Direction {
 
     left, right, top, bottom;
@@ -13,7 +18,21 @@ enum class Direction {
         )
     }
 
+    /**
+     * 方向が水平方向(左右の方向)か否かの値を返す。
+     * @return 水平(左右)である場合true、そうでない場合はfalseを返す。
+     */
+    fun isHorizonal(): Boolean = this == Direction.left || this == Direction.right
 
+    /**
+     * 方向が垂直方向(上下の方向)か否かの値を返す。
+     * @return 垂直(上下)である場合true、そうでない場合はfalseを返す。
+     */
+    fun isVertical(): Boolean = this == Direction.top || this == Direction.bottom
+
+    /**
+     * 矢印オブジェクト(@see Arrow)に変換する
+     */
     fun toArrow(): Arrow {
         return directionToArrow[this]!!
     }
