9766ab7e4ddf174bf9953b20c22612e4c4ae7efe
phinesze <phinesze@gmail.com>
2018-09-17 15:58:37 +0900

    FieldBlockの表示を修正

--- src/game/field/FieldBlock.kt
+++ src/game/field/FieldBlock.kt
@@ -14,7 +14,7 @@ class FieldBlock(var type: FieldBlockType) {
      */
     override fun toString(): String {
         if (gameObjects.size > 0) {
-            for (i in gameObjects.size - 1..0) {
+            for (i in gameObjects.size - 1 downTo 0) {
                 val gameObject = gameObjects[i]
                 return gameObject.display()
             }
