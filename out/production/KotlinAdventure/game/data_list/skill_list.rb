3f138f078f9be987479327b63905552ef1770871
phinesze <phinesze@gmail.com>
2018-09-18 07:17:12 +0900

    gitignore

--- out/production/KotlinAdventure/game/data_list/skill_list.rb
+++
@@ -1,9 +0,0 @@
-require './game/mold/skill'
-include Game::Mold
-
-module Game::DataList
-  skill_list = [
-      Skill.new(name: "ファイアα", time_cost: 1000, mp_cost: 5),
-      Skill.new(name: "アイスα", time_cost: 1000, mp_cost: 5)
-  ]
-end
