79b8e42885d1e97f289c60b8c5cee86f214c316e
phinesze <phinesze@gmail.com>
2018-10-06 10:04:07 +0900

    EnemyMoldのimport文、コメント修正

--- src/game/mold/EnemyMold.kt
+++ src/game/mold/EnemyMold.kt
@@ -2,16 +2,13 @@ package game.mold
 
 import game.NamableObject
 import game.param.AbilityMold
-import game.param.LevelAndExperience
 
 /**
  *  敵キャラクターの種類を表す
  * @property display 自身の所属するFieldのtoStringで出力した際にオブジェクトの表示として表される文字
- * @property abilityMold 能力値を表す型
- * @property levelAndExp レベルとプレイヤーが倒した場合に獲得できる経験値
+ * @property abilityMap 能力値を表す型
  */
 class EnemyMold(name: String,
         var display: String,
         val abilityMap: Map<Int, AbilityMold<Int>>
-//        var levelAndExp: LevelAndExperience
         ): NamableObject(name)
