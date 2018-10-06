1b8dba86cc61ab19378c70c1c971ab5877c1a51b
phinesze <phinesze@gmail.com>
2018-10-06 13:50:10 +0900

    abilityScoreのコメントを追加

--- src/game/param/AbilityScore.kt
+++ src/game/param/AbilityScore.kt
@@ -17,20 +17,49 @@ class AbilityScore(abilityMold: AbilityMold<Int>): Cloneable {
                 this.mp.max = abilityMoldValue.maxMp
             }
 
+    /**
+     * 最大HPと現在HPを表す値
+     */
     val hp = MaxNowValue(abilityMold.maxHp)
 
+    /**
+     * 最大MPと現在MPを表す値
+     */
     val mp = MaxNowValue(abilityMold.maxMp)
 
+    /**
+     * ブーストなどで調整済みの物理攻撃力を取得する。
+     */
     val attack: Int
             get() = this.abilityMold.attack
+
+    /**
+     * ブーストなどで調整済みの物理防御力を取得する。
+     */
     val defense: Int
             get() = this.abilityMold.defense
+
+    /**
+     * ブーストなどで調整済みの魔法攻撃力を取得する。
+     */
     val magicAttack: Int
             get() = this.abilityMold.magicAttack
+
+    /**
+     * ブーストなどで調整済みの魔法防御力を取得する。
+     */
     val magicDefense: Int
             get() = this.abilityMold.magicDefense
+
+    /**
+     * ブーストなどで調整済みの行動力を取得する。
+     */
     val agility: Int
             get() = this.abilityMold.agility
+
+    /**
+     * 倒された場合に倒した側が獲得する経験値を取得する。
+     */
     val droppingExp: Int
             get() = this.abilityMold.droppingExp
 
@@ -40,8 +69,8 @@ class AbilityScore(abilityMold: AbilityMold<Int>): Cloneable {
 
     override fun toString(): String {
         return "HP: ${this.hp}, MP: ${this.mp}," +
-                "attack: ${abilityMold.attack}, defense: ${abilityMold.defense}, " +
-                "magic attack: ${abilityMold.magicAttack}, magic defense: ${abilityMold.magicDefense}, " +
-                "agility: ${abilityMold.agility}"
+                "attack: ${this.attack}, defense: ${this.defense}, " +
+                "magic attack: ${this.magicAttack}, magic defense: ${this.magicDefense}, " +
+                "agility: ${this.agility}"
     }
 }
