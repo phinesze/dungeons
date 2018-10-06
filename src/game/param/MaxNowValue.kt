71b6ca98caa0d40ff8ed7eed67d309188bfffd0f
phinesze <phinesze@gmail.com>
2018-10-06 11:06:17 +0900

    割合回復を追加

--- src/game/param/MaxNowValue.kt
+++ src/game/param/MaxNowValue.kt
@@ -1,19 +1,33 @@
 package game.param
 
+/**
+ * HP、MPのような最大値と現在値のある値を表現する。
+ */
 class MaxNowValue(var max: Int, var now: Int = max) {
 
-    //ダメージを与える。
+    /**
+     * 現在値を減少させる。
+     */
     fun damage(value: Int): Int {
         val damageVal = if (value <= now) value else now
         now -= damageVal
         return damageVal
     }
 
-    //回復する
+    /**
+     * 現在値を回復させる。
+     */
     fun restore(value: Int): Int {
         val restoreVal = if (value <= max - now) value else max - now
         now += restoreVal
         return restoreVal
+    }
+
+    /**
+     * 最大値に対する割合を指定して回復させる。
+     */
+    fun restoreRatio(ratio: Double): Int {
+        return restore((max * ratio).toInt())
     }
 
     override fun toString(): String = "${now}/${max}"
